9e178885e97b08f0317f7795b1f9d3dbad0e7798
cachu <adrianfer_87@hotmail.com>
2023-05-04 18:30:20 -0300

    finalizandoentrega

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,35 +1,63 @@
-const entradas=document.querySelectorAll('.fuente-botones')
+const entradas = document.querySelectorAll("#mi-boton");
+const inputs = document.querySelectorAll('input');
+const mensaje = document.querySelector('textarea');
+const formulario = document.querySelector('#mi-formulario');
 
-function respuestaClick1(){
-    alert("Recibimos tu mensaje, te responderemos a la brevedad")
+
+entradas[0].addEventListener('click', respuestaClick1);
+entradas[1].addEventListener('click', respuestaClick2);
+
+const formnombre = inputs[0];
+const correo = inputs[1];
+const telefono = inputs[2];
+
+formnombre.addEventListener('keyup', () => {
+  console.log(inputs[0].value);
+});
+
+correo.addEventListener('keyup', () => {
+  console.log(inputs[1].value);
+});
+
+telefono.addEventListener('keyup', () => {
+  console.log(inputs[2].value);
+});
+
+mensaje.addEventListener('keyup', () => {
+  console.log(mensaje.value);
+});
+
+const boton = document.querySelector('#mi-boton');
+
+function respuestaClick1() {
+  console.log("Su mensaje ha sido enviado");
+  event.preventDefault();
+  localStorage.setItem('nombre y apellido', formnombre.value);
+  localStorage.setItem('correo', correo.value);
+  localStorage.setItem('teléfono', telefono.value);
+  localStorage.setItem('mensaje', mensaje.value);
+  Swal.fire({
+    title: 'mensaje enviado',
+    timer: 3000,
+    timerProgressBar: true,
+  });
+formnombre.value = "";
+correo.value = "";
+telefono.value = "";
+mensaje.value = "";
+Swal.fire({
+title: 'mensaje enviado',
+timer: 3000,
+timerProgressBar: true,
+});
 }
-function respuestaClick2(){
-    console.log("Usuario borró los datos")
+
+// Boton borrar datos
+function respuestaClick2() {
+  console.log("Usuario borró los datos");
+  Swal.fire({
+    title: 'Ha borrado sus datos',
+    timer: 3000,
+    timerProgressBar: true,
+  });
 }
-
-entradas[0].addEventListener('click', respuestaClick1)
-entradas[1].addEventListener('click', respuestaClick2)
-
-const inputs=document.querySelectorAll('input')
-const mensaje=document.querySelector('textarea')
-
-const formnombre = inputs[0]
-const correo = inputs[1]
-const telefono = inputs[2]
-
-formnombre.addEventListener('keyup', ()=>{
-    console.log(inputs[0].value)
-})
-
-correo.addEventListener('keyup',()=>{
-    console.log(inputs[1].value)
-})
-
-telefono.addEventListener('keyup',()=>{
-    console.log(inputs[2].value)
-})
-
-mensaje.addEventListener('keyup', ()=>{
-    console.log(mensaje.value)
-})
-
